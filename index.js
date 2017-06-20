6bf2681a9db6d0e489415ab5052cf1be179f20bf
Febry Antonius <febry@yesbossnow.com>
2017-06-20 11:00:26 +0700

    Add superagent to package and resolve path for service.yml and package.json file

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const path = require("path");
 const merapi = require("merapi");
 const yaml = require("js-yaml");
 const fs = require("fs");
-const config =  yaml.safeLoad(fs.readFileSync("./service.yml", "utf8"));
-config.package = require("./package");
+const config =  yaml.safeLoad(fs.readFileSync(`${__dirname}/service.yml`, "utf8"));
+config.package = require(`${__dirname}/package`);
 
 module.exports = merapi({
     basepath: path.resolve(__dirname, "lib"),
