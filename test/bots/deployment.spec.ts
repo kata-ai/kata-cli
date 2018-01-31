5a3f8666330516039b4cc6210a290b9577db04ee
prakashdivyy <prakashdivyy@gmail.com>
2018-01-31 13:04:16 +0700

    Fix unit testing

--- test/bots/deployment.spec.ts
+++ test/bots/deployment.spec.ts
@@ -12,118 +12,112 @@ import { v4 as uuid } from "node-uuid";
 const Table = require("cli-table");
 
 @suite class DeploymentTest {
-    private config: IConfig;
-    private helper: IHelper;
-    private api: any;
-    private deployment: any;
+    private config : IConfig;
+    private helper : IHelper;
+    private api : any;
+    private deployment : any;
     private emptyDeploymentObj = {
         name: "test",
         botId: "739b5e9f-d5e1-44b1-93a8-954d291df170",
         botVersion: "1.0.5",
         channels: {}
-    }
+    };
     private deploymentObj = {
         name: "test",
         botId: "739b5e9f-d5e1-44b1-93a8-954d291df170",
         botVersion: "1.0.5",
         channels: {
-            "fb": "a22867dd-ce49-4afe-b7d1-3199c01e1c51",
-            "line": "b02eb207-8f8a-480d-8c32-606b0fa7dfe7"
+            fb: "a22867dd-ce49-4afe-b7d1-3199c01e1c51",
+            line: "b02eb207-8f8a-480d-8c32-606b0fa7dfe7"
         }
     };
     private channelObj = {
         name: "fb",
-        id: this.deploymentObj.channels["fb"],
+        id: this.deploymentObj.channels.fb,
         type: "messenger",
         token: "tokenChannel",
         refreshToken: "refreshToken",
         secret: "secretKey",
         url: "http://url",
         webhook: "https://urlwebhook"
-    }
+    };
 
     private channelObjWithOptions = {
         name: "fb",
-        id: this.deploymentObj.channels["fb"],
+        id: this.deploymentObj.channels.fb,
         type: "messenger",
         token: "tokenChannel",
         refreshToken: "refreshToken",
         secret: "secretKey",
         url: "http://url",
-        additionalOptions: { "botEmail": 'test@test.com' },
+        additionalOptions: { botEmail: "test@test.com" },
         webhook: "https://urlwebhook"
-    }
+    };
     private webhook = "https://kanal.katalabs.io";
 
     constructor() {
-        let configJson = safeLoad(readFileSync("./service.yml", "utf8"));
-        let zaun = Zaun();
+        const configJson = safeLoad(readFileSync("./service.yml", "utf8"));
+        const zaun = Zaun();
         this.config = Config.create(configJson);
         this.helper = new Helper(this.config);
         this.api = new Api(this.helper, zaun);
         this.deployment = new Deployment(this.helper, this.api, this.config);
     }
 
-    @test async "function deploy should create deployment successfully"() {
-        let createdDeploymentId = uuid();
-        let optsCreateDeployment = {
+    @test public async "function deploy should create deployment successfully"() {
+        const createdDeploymentId = uuid();
+        const optsCreateDeployment = {
             body: {
                 name: this.deploymentObj.name,
                 botVersion: this.deploymentObj.botVersion,
                 channels: {}
             }
         };
-        let getBotIdStub = stub(this.helper, "getBotId").returns(this.deploymentObj.botId);
-        let getBotVersionStub = stub(this.api.botApi, "botsBotIdVersionsGet").callsFake((botId, callback) => {
+        const getBotIdStub = stub(this.helper, "getBotId").returns(this.deploymentObj.botId);
+        const getBotVersionStub = stub(this.api.botApi, "botsBotIdVersionsGet").callsFake((botId, callback) => {
             callback(null, { versions: [this.deploymentObj.botVersion], latest: this.deploymentObj.botVersion });
         });
-        let getDeploymentStub = stub(this.api.deploymentApi, "botsBotIdDeploymentsDeploymentIdGet").callsFake((botId, deploymentId, callback) => {
-            callback(new Error("Deployment not found."));
-        });
-        let createDeploymentStub = stub(this.api.deploymentApi, "botsBotIdDeploymentsPost").callsFake((botId, opts, callback) => {
+        const createDeploymentStub = stub(this.api.deploymentApi, "botsBotIdDeploymentsPost").callsFake((botId, opts, callback) => {
             callback(null, { ...this.deploymentObj, channels: {}, id: createdDeploymentId });
         });
-        let consoleLogStub = stub(console, "log");
-        let consoleDirStub = stub(console, "dir");
+        const consoleLogStub = stub(console, "log");
+        const consoleDirStub = stub(console, "dir");
 
         await this.deployment.deploy(this.deploymentObj.name, this.deploymentObj.botVersion, {});
 
         getBotIdStub.restore();
         getBotVersionStub.restore();
-        getDeploymentStub.restore();
         createDeploymentStub.restore();
         consoleLogStub.restore();
         consoleDirStub.restore();
         assert.calledOnce(getBotVersionStub);
-        assert.calledOnce(getDeploymentStub);
         assert.calledOnce(createDeploymentStub);
         assert.calledOnce(consoleLogStub);
         assert.calledOnce(consoleDirStub);
         assert.calledWith(getBotVersionStub, this.deploymentObj.botId);
-        assert.calledWith(getDeploymentStub, this.deploymentObj.botId, this.deploymentObj.name);
         assert.calledWith(createDeploymentStub, this.deploymentObj.botId, optsCreateDeployment);
         assert.calledWith(consoleLogStub, "DEPLOYMENT CREATED SUCCESSFULLY");
-        assert.calledWith(consoleDirStub, { ...this.deploymentObj, channels: {}, id: createdDeploymentId, tag: null });;
+        assert.calledWith(consoleDirStub, { ...this.deploymentObj, channels: {}, id: createdDeploymentId, tag: null });
     }
 
-    @test async "function deploy should update deployment successfully if deployment has been created"() {
-        let createdDeploymentId = uuid();
-        let body = {
+    @test public async "function deploy should update deployment successfully if deployment has been created"() {
+        const createdDeploymentId = uuid();
+        const body = {
             name: this.deploymentObj.name,
             botVersion: this.deploymentObj.botVersion
         };
-        let getBotIdStub = stub(this.helper, "getBotId").returns(this.deploymentObj.botId);
-        let getBotVersionStub = stub(this.api.botApi, "botsBotIdVersionsGet").callsFake((botId, callback) => {
+        const getBotIdStub = stub(this.helper, "getBotId").returns(this.deploymentObj.botId);
+        const getBotVersionStub = stub(this.api.botApi, "botsBotIdVersionsGet").callsFake((botId, callback) => {
             callback(null, { versions: ["1.0.0", "1.0.1", this.deploymentObj.botVersion], latest: this.deploymentObj.botVersion });
         });
-        let getDeploymentStub = stub(this.api.deploymentApi, "botsBotIdDeploymentsDeploymentIdGet").callsFake((botId, deploymentId, callback) => {
+        const getDeploymentStub = stub(this.api.deploymentApi, "botsBotIdDeploymentsDeploymentIdGet").callsFake((botId, deploymentId, callback) => {
             callback(null, this.deploymentObj);
         });
-        let updateDeploymentStub = stub(this.api.deploymentApi, "botsBotIdDeploymentsDeploymentIdPut").callsFake((botId, deploymentId, body, callback) => {
+        const updateDeploymentStub = stub(this.api.deploymentApi, "botsBotIdDeploymentsDeploymentIdPut").callsFake((botId, deploymentId, body, callback) => {
             callback(null, { ...this.deploymentObj, id: createdDeploymentId });
         });
-        let consoleLogStub = stub(console, "log");
-        let consoleDirStub = stub(console, "dir");
+        const consoleLogStub = stub(console, "log");
+        const consoleDirStub = stub(console, "dir");
 
         await this.deployment.deploy(this.deploymentObj.name, null, {});
 
@@ -145,20 +139,20 @@ const Table = require("cli-table");
         assert.calledWith(consoleDirStub, { ...this.deploymentObj, id: createdDeploymentId, tag: "latest" });
     }
 
-    @test async "function deploy throw error if bot with botVersion is undefined"() {
-        let createdDeploymentId = uuid();
-        let optsCreateDeployment = {
+    @test public async "function deploy throw error if bot with botVersion is undefined"() {
+        const createdDeploymentId = uuid();
+        const optsCreateDeployment = {
             body: {
                 name: this.deploymentObj.name,
                 botVersion: this.deploymentObj.botVersion,
                 channels: {}
             }
         };
-        let getBotIdStub = stub(this.helper, "getBotId").returns(this.deploymentObj.botId);
-        let getBotVersionStub = stub(this.api.botApi, "botsBotIdVersionsGet").callsFake((botId, callback) => {
+        const getBotIdStub = stub(this.helper, "getBotId").returns(this.deploymentObj.botId);
+        const getBotVersionStub = stub(this.api.botApi, "botsBotIdVersionsGet").callsFake((botId, callback) => {
             callback(null, { versions: [this.deploymentObj.botVersion], latest: this.deploymentObj.botVersion });
         });
-        let consoleLogStub = stub(console, "log");
+        const consoleLogStub = stub(console, "log");
 
         await this.deployment.deploy(this.deploymentObj.name, "1.0.6", {});
 
@@ -171,19 +165,19 @@ const Table = require("cli-table");
         assert.calledWith(consoleLogStub, "INVALID TAG");
     }
 
-    @test async "function add channel with additional options should add channel to deployment successfully"() {
-        let getBotIdStub = stub(this.helper, "getBotId").returns(this.deploymentObj.botId);
-        let getDeploymentStub = stub(this.api.deploymentApi, "botsBotIdDeploymentsDeploymentIdGet").callsFake((botId, deploymentId, callback) => {
+    @test public async "function add channel with additional options should add channel to deployment successfully"() {
+        const getBotIdStub = stub(this.helper, "getBotId").returns(this.deploymentObj.botId);
+        const getDeploymentStub = stub(this.api.deploymentApi, "botsBotIdDeploymentsDeploymentIdGet").callsFake((botId, deploymentId, callback) => {
             callback(null, this.emptyDeploymentObj);
         });
-        let createChannelStub = stub(this.api.channelApi, "botsBotIdDeploymentsDeploymentIdChannelsPost").callsFake((body, botId, deploymentId, callback) => {
+        const createChannelStub = stub(this.api.channelApi, "botsBotIdDeploymentsDeploymentIdChannelsPost").callsFake((body, botId, deploymentId, callback) => {
             callback(null, this.channelObjWithOptions);
-        })
-        let consoleLogStub = stub(console, "log");
-        let channels: { [name: string]: string } = {};
+        });
+        const consoleLogStub = stub(console, "log");
+        const channels : { [name : string] : string } = {};
         channels[this.channelObjWithOptions.name] = this.channelObjWithOptions.id;
 
-        let channelData = {
+        const channelData = {
             id: this.channelObjWithOptions.id,
             name: this.channelObjWithOptions.name,
             type: this.channelObjWithOptions.type,
@@ -209,12 +203,12 @@ const Table = require("cli-table");
         assert.calledWith(consoleLogStub, `Paste this url to ${channelData.type} webhook : ${this.channelObjWithOptions.webhook}`);
     }
 
-    @test async "function add channel should show error if channel name added has been used"() {
-        let getBotIdStub = stub(this.helper, "getBotId").returns(this.deploymentObj.botId);
-        let getDeploymentStub = stub(this.api.deploymentApi, "botsBotIdDeploymentsDeploymentIdGet").callsFake((botId, deploymentId, callback) => {
+    @test public async "function add channel should show error if channel name added has been used"() {
+        const getBotIdStub = stub(this.helper, "getBotId").returns(this.deploymentObj.botId);
+        const getDeploymentStub = stub(this.api.deploymentApi, "botsBotIdDeploymentsDeploymentIdGet").callsFake((botId, deploymentId, callback) => {
             callback(null, this.deploymentObj);
         });
-        let consoleLogStub = stub(console, "log");
+        const consoleLogStub = stub(console, "log");
 
         await this.deployment.addChannel(this.deploymentObj.name, "fb", {});
 
@@ -225,15 +219,15 @@ const Table = require("cli-table");
         assert.calledWith(consoleLogStub, "CHANNEL NAME HAS BEEN USED");
     }
 
-    @test async "function remove channel should remove channel successfully"() {
-        let getBotIdStub = stub(this.helper, "getBotId").returns(this.deploymentObj.botId);
-        let getDeploymentStub = stub(this.api.deploymentApi, "botsBotIdDeploymentsDeploymentIdGet").callsFake((botId, deploymentId, callback) => {
+    @test public async "function remove channel should remove channel successfully"() {
+        const getBotIdStub = stub(this.helper, "getBotId").returns(this.deploymentObj.botId);
+        const getDeploymentStub = stub(this.api.deploymentApi, "botsBotIdDeploymentsDeploymentIdGet").callsFake((botId, deploymentId, callback) => {
             callback(null, this.deploymentObj);
         });
-        let removeChannelStub = stub(this.api.channelApi, "botsBotIdDeploymentsDeploymentIdChannelsChannelIdDelete").callsFake((botId, deploymentId, channelId, callback) => {
+        const removeChannelStub = stub(this.api.channelApi, "botsBotIdDeploymentsDeploymentIdChannelsChannelIdDelete").callsFake((botId, deploymentId, channelId, callback) => {
             callback();
         });
-        let consoleLogStub = stub(console, "log");
+        const consoleLogStub = stub(console, "log");
 
         await this.deployment.removeChannel(this.deploymentObj.name, "fb", {});
 
@@ -248,12 +242,12 @@ const Table = require("cli-table");
         assert.calledWith(consoleLogStub, "CHANNEL REMOVED SUCCESSFULLY");
     }
 
-    @test async "function remove channel should throw error if channel not found in deployment"() {
-        let getBotIdStub = stub(this.helper, "getBotId").returns(this.deploymentObj.botId);
-        let getDeploymentStub = stub(this.api.deploymentApi, "botsBotIdDeploymentsDeploymentIdGet").callsFake((botId, deploymentId, callback) => {
+    @test public async "function remove channel should throw error if channel not found in deployment"() {
+        const getBotIdStub = stub(this.helper, "getBotId").returns(this.deploymentObj.botId);
+        const getDeploymentStub = stub(this.api.deploymentApi, "botsBotIdDeploymentsDeploymentIdGet").callsFake((botId, deploymentId, callback) => {
             callback(null, this.emptyDeploymentObj);
         });
-        let consoleLogStub = stub(console, "log");
+        const consoleLogStub = stub(console, "log");
 
         await this.deployment.removeChannel(this.deploymentObj.name, "fb", {});
 
@@ -264,12 +258,12 @@ const Table = require("cli-table");
         assert.calledWith(consoleLogStub, "CHANNEL NOT FOUND");
     }
 
-    @test async "should call zaun api to drop deployment"() {
-        let getBotIdStub = stub(this.helper, "getBotId").returns(this.deploymentObj.botId);
-        let deploymentApiDeleteStub = stub(this.api.deploymentApi, "botsBotIdDeploymentsDeploymentIdDelete").callsFake((botId, deploymentId, callback) => {
+    @test public async "should call zaun api to drop deployment"() {
+        const getBotIdStub = stub(this.helper, "getBotId").returns(this.deploymentObj.botId);
+        const deploymentApiDeleteStub = stub(this.api.deploymentApi, "botsBotIdDeploymentsDeploymentIdDelete").callsFake((botId, deploymentId, callback) => {
             callback(null, this.deploymentObj);
         });
-        let consoleStub = stub(console, "log");
+        const consoleStub = stub(console, "log");
 
         await this.deployment.drop(this.deploymentObj.name, {});
 
@@ -281,16 +275,16 @@ const Table = require("cli-table");
         assert.calledWith(consoleStub, "DEPLOYMENT DELETED SUCCESSFULLY");
     }
 
-    @test async "should call zaun api to list deployment"() {
-        let getBotIdStub = stub(this.helper, "getBotId").returns(this.deploymentObj.botId);
-        let deploymentApiDeleteStub = stub(this.api.deploymentApi, "botsBotIdDeploymentsGet").callsFake((botId, { }, callback) => {
+    @test public async "should call zaun api to list deployment"() {
+        const getBotIdStub = stub(this.helper, "getBotId").returns(this.deploymentObj.botId);
+        const deploymentApiDeleteStub = stub(this.api.deploymentApi, "botsBotIdDeploymentsGet").callsFake((botId, { }, callback) => {
             callback(null, null, { body: [this.deploymentObj] });
         });
-        let consoleStub = stub(console, "log");
+        const consoleStub = stub(console, "log");
         await this.deployment.list();
 
-        let table = new Table({
-            head: ['Deployment Name', 'Version'],
+        const table = new Table({
+            head: ["Deployment Name", "Version"],
             colWidths: [30, 10]
         });
         table.push([this.deploymentObj.name, this.deploymentObj.botVersion]);
