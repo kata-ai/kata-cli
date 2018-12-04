803bd013393fe0fb4a2b75861b6e36bf241d31d6
Rizky Luthfianto <mrluthfianto@gmail.com>
2018-12-04 17:33:42 +0700

    Fix environment

--- components/bots/environment.ts
+++ components/bots/environment.ts
@@ -11,14 +11,21 @@ export default class Environment {
 
     public async create(slug: string) {
         const projectId = this.helper.getProjectId();
-        const postBody = {
-            depId: projectId,
-            name: "Development",
-            depVersion: "0.0.1",
-            slug
-        };
 
         try {
+            const deployment = await this.getLatestDeployment();
+            if (!deployment) {
+                throw Error("Deployment not found");
+            }
+
+            const name = await this.askPredefinedEnvironmentName();
+            
+            const postBody = {
+                depId: projectId,
+                depVersion: deployment.version,
+                name, slug,
+            };
+
             const { response: { body } } = await this.helper.toPromise(
                 this.api.deploymentApi, this.api.deploymentApi.projectsProjectIdEnvironmentsPost,
                 projectId, postBody,
@@ -57,23 +64,23 @@ export default class Environment {
             const environmentId = await this.askEnvironmentId({
                 message: "Select which Environment to update: "
             });
-            console.log(newDeploymentVersion)
+
             const { response: { body } } = await this.helper.toPromise(
                 this.api.deploymentApi, this.api.deploymentApi.projectsProjectIdEnvironmentsEnvironmentIdPut,
                 environmentId, projectId, { depVersion: newDeploymentVersion }
             );
 
             if (!body) {
-                throw Error("error");
+                throw Error("Error updating Environment");
             }
 
-            return console.log(body)
+            return console.log(body);
         } catch (error) {
             console.log(this.helper.wrapError(error));
         }
     }
 
-    public async askEnvironmentId(prop?: {message?: string}): Promise<string> {
+    public async askEnvironmentId(prop?: { message?: string }): Promise<string> {
         const environmentList = await this.listEnvironment();
         const choices = environmentList.map((row: any) => ({ name: `${row.name} (${row.depVersion})`, value: row.id }));
 
@@ -88,6 +95,33 @@ export default class Environment {
         ]);
         return environmentId;
     }
+
+    private async askPredefinedEnvironmentName(): Promise<string> {
+        const environmentNames = ["Development", "Production", "Staging"];
+        const choices = environmentNames.map((name) => ({ name, value: name }));
+
+        const { environmentName } = await inquirer.prompt<any>([
+            {
+                type: "list",
+                name: "environmentName",
+                message: "Which Environment to create?",
+                paginated: false,
+                choices
+            },
+        ]);
+
+        return environmentName;
+    }
+
+    private async getLatestDeployment() {
+        const projectId = this.helper.getProjectId();
+        console.log(projectId)
+        const { response: { body: latestDeployment } } = await this.helper.toPromise(
+            this.api.projectApi, this.api.projectApi.projectsProjectIdDeploymentGet, projectId
+        );
+        return latestDeployment;
+    }
+
     private async listEnvironment(): Promise<any[]> {
         const projectId = this.helper.getProp("projectId");
         const { response: { body } } = await this.helper.toPromise(
