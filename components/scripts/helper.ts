dd0c24c9a453901e8c03010d981383b6eb683dc0
oktavianidewi <oktaviani.dewi@gmail.com>
2019-08-24 11:18:22 +0700

    give any type

--- components/scripts/helper.ts
+++ components/scripts/helper.ts
@@ -211,9 +211,9 @@ export default class Helper extends Component {
         return errorMessage;
     }
 
-    public difference(object : any, base : any) {
-        function changes(object : any, base : any) {
-            return _.transform(object, function(result, value, key) {
+    public difference(object: any, base: any) {
+        function changes(object: any, base: any) {
+            return _.transform(object, function(result: any, value, key) {
                 if (!_.isEqual(value, base[key])) {
                     result[key] = (_.isObject(value) && _.isObject(base[key])) ? changes(value, base[key]) : value;
                 }
