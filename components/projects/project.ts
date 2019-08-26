872f950212e0770dfce1808f6710f17ddd7bcc15
oktavianidewi <oktaviani.dewi@gmail.com>
2019-08-26 14:06:44 +0700

    Give condition for empty projectName

--- components/projects/project.ts
+++ components/projects/project.ts
@@ -221,7 +221,7 @@ export default class Project {
 
     public async select(projectName?: string) {
         let chosen: any;
-        if (projectName || typeof projectName === "string") {
+        if (typeof projectName === "string") {
             chosen = await this.getDataByName(projectName);
             if (chosen !== undefined) {
                 // project name found
@@ -229,7 +229,9 @@ export default class Project {
                 this.helper.setProp("projectName", chosen.name);
             } else {
                 // project name not found, select through inquirer
-                console.log(`Project with name ${projectName} is not found. Instead, please choose listed project name below:`);
+                console.log(
+                    `Project with name ${colors.green(projectName)} is not found. ` +
+                    `Please choose listed project name below:`);
                 chosen = await this.choose()
                 if (chosen) {
                     this.helper.setProp("projectId", chosen.id);
--- components/scripts/helper.ts
+++ components/scripts/helper.ts
@@ -213,7 +213,7 @@ export default class Helper extends Component {
 
     public difference(object : any, base : any) {
         function changes(object : any, base : any) {
-            return _.transform(object, function(result, value, key) {
+            return _.transform(object, function(result: any, value, key) {
                 if (!_.isEqual(value, base[key])) {
                     result[key] = (_.isObject(value) && _.isObject(base[key])) ? changes(value, base[key]) : value;
                 }
