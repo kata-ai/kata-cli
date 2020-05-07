47e33b50ecf4708a43ba5cccfd46a41490cc9714
oktaviani.dewi <oktaviani.dewi@gmail.com>
2020-05-08 05:44:53 +0700

    Add try catch to handle training rejection

--- components/nlus/nlu.ts
+++ components/nlus/nlu.ts
@@ -292,36 +292,40 @@ export default class Nlu extends Component {
                 };
             }
 
-            const { response: { body } } = await this.helper.toPromise(this.api.nluApi,
-                this.api.nluApi.projectsProjectIdNlusNluNameTrainPost, projectId, nluName, opts);
-            const trainResult = body;
-
-            // Print result
-            const count = trainResult.count;
-            const successCount = trainResult.rowIds ? trainResult.rowIds.length : 0;
-            if (successCount) {
-                console.log(`Success: ${successCount} data trained !`);
-            }
-
-            // Write error to file
-            const errorCount = trainResult.errRows ? trainResult.errRows.length : 0;
-            if (errorCount) {
-                if (options.file) {
-                    const rawData = fs.readFileSync(options.file).toString("utf8");
-                    const trainingData = rawData.split("\n");
-                    let errData = "";
-                    for (const i of trainResult.errRows) {
-                        errData += trainingData[i] + "\n";
-                    }
-                    fs.writeFile(errorFileLog, errData, (err: any) => {
-                        if (err) { throw err; }
-                        console.log(`Error training ${errorCount} data. See details on ${errorFileLog}`);
-                    });
-                } else if (options.sentence) {
-                    console.log(`Error training data`);
+            // saat proses training lama, misal 30k data, proses training berhenti/reject ada balikan error tapi tidak dihandle.
+            try {
+                const { response: { body } } = await this.helper.toPromise(this.api.nluApi,
+                    this.api.nluApi.projectsProjectIdNlusNluNameTrainPost, projectId, nluName, opts);
+                const trainResult = body;
+    
+                // Print result
+                const count = trainResult.count;
+                const successCount = trainResult.rowIds ? trainResult.rowIds.length : 0;
+                if (successCount) {
+                    console.log(`Success: ${successCount} data trained !`);
                 }
+    
+                // Write error to file
+                const errorCount = trainResult.errRows ? trainResult.errRows.length : 0;
+                if (errorCount) {
+                    if (options.file) {
+                        const rawData = fs.readFileSync(options.file).toString("utf8");
+                        const trainingData = rawData.split("\n");
+                        let errData = "";
+                        for (const i of trainResult.errRows) {
+                            errData += trainingData[i] + "\n";
+                        }
+                        fs.writeFile(errorFileLog, errData, (err: any) => {
+                            if (err) { throw err; }
+                            console.log(`Error training ${errorCount} data. See details on ${errorFileLog}`);
+                        });
+                    } else if (options.sentence) {
+                        console.log(`Error training data`);
+                    }
+                }
+            } catch (error) {
+                console.log(`Process training data is rejected. Error details : ${JSON.stringify(error)}`);
             }
-
         } catch (error) {
             console.log(this.helper.wrapError(error));
         }
